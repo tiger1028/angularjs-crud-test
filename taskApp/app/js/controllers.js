82713e5f77a7c433ebee2b09500f80a597194dac
tiger1028 <gentleman.tiger1028@gmail.com>
2014-04-07 20:26:11 +0200

    angularjs-crud-test

--- taskApp/app/js/controllers.js
+++ taskApp/app/js/controllers.js
@@ -4,12 +4,12 @@
 
 var tasksControllers = angular.module('tasksControllers', []);
 
-var rooturl = '/angularjs-crud-test';
+var api_root = '/angularjs-crud-test/taskAPI';
 
 tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 
 	$scope.loadData = function(orderProp='taskId'){
-		$http.get(rooturl + '/taskAPI/task/get').success(function(data) {
+		$http.get(api_root + '/task/get').success(function(data) {
 			$scope.tasks = data;
 			console.log(data);
 		});
@@ -18,7 +18,7 @@ tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 
 	$scope.addTask = function() {
 		var data = { task: $scope.task, status: "1" };
-		$http.post(rooturl + '/taskAPI/task/add', data).success(function (data, status) {
+		$http.post(api_root + '/task/add', data).success(function (data, status) {
 			$scope.loadData();
 			// Alternative: $scope.tasks.push.apply($scope.tasks, data);
 			console.log(data);
@@ -28,7 +28,7 @@ tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 	$scope.deleteTask = function(taskId) {
 		var data = { taskId: taskId };
 		if(confirm('Delete task ' + taskId + '?','Please confirm')){
-			$http.post(rooturl + '/taskAPI/task/delete', data).success(function (data, status){
+			$http.post(api_root + '/task/delete', data).success(function (data, status){
 				$scope.loadData();
 				console.log(data);
 			});
@@ -37,7 +37,7 @@ tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 
 	$scope.updateTask = function(taskId, task, status) {
 		var data = { taskId: taskId, task: task, status: status };
-		$http.post(rooturl + '/taskAPI/task/update', data).success(function (data, status) {
+		$http.post(api_root + '/task/update', data).success(function (data, status) {
 			$scope.loadData();
 			console.log(data);
 		});
@@ -50,7 +50,7 @@ tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 tasksControllers.controller('TaskDetailCtrl', function ($scope, $routeParams, $http){
 
 	$scope.loadTask = function(){
-		$http.get(rooturl + '/taskAPI/task/get/' + $routeParams.taskId).success(function(data) {
+		$http.get(api_root + '/task/get/' + $routeParams.taskId).success(function(data) {
 			$scope.taskDetail = data[0];
 
 			console.log($scope.taskDetail);
@@ -59,7 +59,7 @@ tasksControllers.controller('TaskDetailCtrl', function ($scope, $routeParams, $h
 
 	$scope.updateTask = function() { // taskId, task, status
 		var data = { taskId: $scope.taskDetail.taskId, task: $scope.taskDetail.task, status: $scope.taskDetail.status };
-		$http.post(rooturl + '/taskAPI/task/update', data).success(function (data, status) {
+		$http.post(api_root + '/task/update', data).success(function (data, status) {
 			console.log(data);
 		});
 	}
