cd400d9caeaecc3c4810051badbab31754363cff
tiger1028 <gentleman.tiger1028@gmail.com>
2014-04-07 19:00:56 +0200

    angularjs-crud-test

--- taskApp/app/js/controllers.js
+++ taskApp/app/js/controllers.js
@@ -4,39 +4,42 @@
 
 var tasksControllers = angular.module('tasksControllers', []);
 
+var rooturl = '/angularjs-crud-test';
+
 tasksControllers.controller('TasksListCtrl', function ($scope, $http){
 
 	$scope.loadData = function(orderProp='taskId'){
-		$http.get('/angularjs-crud-test/taskAPI/task/get').success(function(data) {
+		$http.get(rooturl + '/taskAPI/task/get').success(function(data) {
 			$scope.tasks = data;
+			console.log(data);
 		});
 		$scope.orderProp = orderProp;
 	}
 
 	$scope.addTask = function() {
 		var data = { task: $scope.task, status: "1" };
-		$http.post("/angularjs-crud-test/taskAPI/task/add", data).success(function (data, status) {
-			// console.log(data);
-			// $scope.tasks.push.apply($scope.tasks, data);
+		$http.post(rooturl + '/taskAPI/task/add', data).success(function (data, status) {
 			$scope.loadData();
+			// Alternative: $scope.tasks.push.apply($scope.tasks, data);
+			console.log(data);
 		});
 	}
 
 	$scope.deleteTask = function(taskId) {
 		var data = { taskId: taskId };
-		if(confirm('Delete task ' + taskId + '?','Question')){
-			$http.post("/angularjs-crud-test/taskAPI/task/delete", data).success(function (data, status){
+		if(confirm('Delete task ' + taskId + '?','Please confirm')){
+			$http.post(rooturl + '/taskAPI/task/delete', data).success(function (data, status){
 				$scope.loadData();
+				console.log(data);
 			});
     	}
 	}
 
 	$scope.updateTask = function(taskId, task, status) {
 		var data = { taskId: taskId, task: task, status: status };
-		$http.post("/angularjs-crud-test/taskAPI/task/update", data).success(function (data, status) {
-			console.log(data);
+		$http.post(rooturl + '/taskAPI/task/update', data).success(function (data, status) {
 			$scope.loadData();
-			//$scope.tasks.push.apply($scope.tasks, data);
+			console.log(data);
 		});
 	}
 
